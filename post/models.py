bf0118e7ad899acb795abdce09027edb7321a1f1
Laura <lauradhamilton@gmail.com>
2014-09-06 11:33:01 -0500

    Fixing upload_to photos reference

--- post/models.py
+++ post/models.py
@@ -7,6 +7,6 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     author = models.ForeignKey(User)
     content = models.TextField()
-    photo = models.ImageField(upload_to='/photos/')
+    photo = models.ImageField(upload_to='photos')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
