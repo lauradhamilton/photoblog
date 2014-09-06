a4718b8dcb011bcbd04cdae449655836ed1d6372
Laura <lauradhamilton@gmail.com>
2014-09-06 12:35:00 -0500

    Displaying the posts on the /post/ page

--- photoblog/urls.py
+++ photoblog/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^post/$', include('post.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
