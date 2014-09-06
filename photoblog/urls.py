59697e5ed28268b4a505927f2fa5bb7766b28b3e
Laura <lauradhamilton@gmail.com>
2014-09-06 13:07:21 -0500

    Adding a detail page for each post

--- photoblog/urls.py
+++ photoblog/urls.py
@@ -8,6 +8,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^post/$', include('post.urls')),
+    url(r'^post/', include('post.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- post/views.py
+++ post/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 from post.models import Post
 
 def index(request):
-   posts = Post.objects.all()
-   context = {'posts': posts}
-   return render(request, 'index.html', context) 
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'index.html', context)
+
+def post(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'post.html', context) 
