1657cff8edb5b427adcce5c2b71ef2ccd01d83a7
Laura <lauradhamilton@gmail.com>
2014-09-06 11:04:09 -0500

    Adding a post model

---
+++ post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='/photos/')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
